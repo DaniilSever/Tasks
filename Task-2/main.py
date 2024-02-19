3d1f7df48db323cfb39ebdf0011afdc7d2d340b9
Server <server@localhost.localdomain>
2024-02-19 03:14:27 -0500

    repair

--- Task-2/main.py
+++ Task-2/main.py
@@ -12,43 +12,44 @@
 
 class TriangleChecker:
     def __init__(self, first_value: int, second_value: int, third_value: int):
-        if (
-            isinstance(first_value, int)
-            and isinstance(second_value, int)
-            and isinstance(third_value, int)
-        ):
-            self.__first_rib = first_value
-            self.__second_rib = second_value
-            self.__third_rib = third_value
-        else:
-            raise TypeError("Нужно вводить только числа!")
+        self.__first_rib = first_value
+        self.__second_rib = second_value
+        self.__third_rib = third_value
+    
 
     def is_triangle(self):
-        if self.__first_rib > 0 and self.__second_rib > 0 and self.__third_rib > 0:
-            max_rib = max(self.__first_rib, self.__second_rib, self.__third_rib)
-            match max_rib:
-                case self.__first_rib:
-                    res = self.__second_rib + self.__third_rib
-                    if res > max_rib:
-                        print("Ура, можно построить треугольник!")
-                    else:
-                        print("Жаль, но из этого треугольник не сделать")
+        if (
+            isinstance(self.__first_rib, int)
+            and isinstance(self.__second_rib, int)
+            and isinstance(self.__third_rib, int)
+        ):
+            if self.__first_rib > 0 and self.__second_rib > 0 and self.__third_rib > 0:
+                max_rib = max(self.__first_rib, self.__second_rib, self.__third_rib)
+                match max_rib:
+                    case self.__first_rib:
+                        res = self.__second_rib + self.__third_rib
+                        if res > max_rib:
+                            print("Ура, можно построить треугольник!")
+                        else:
+                            print("Жаль, но из этого треугольник не сделать")
 
-                case self.__second_rib:
-                    res = self.__first_rib + self.__third_rib
-                    if res > max_rib:
-                        print("Ура, можно построить треугольник!")
-                    else:
-                        print("Жаль, но из этого треугольник не сделать")
+                    case self.__second_rib:
+                        res = self.__first_rib + self.__third_rib
+                        if res > max_rib:
+                            print("Ура, можно построить треугольник!")
+                        else:
+                            print("Жаль, но из этого треугольник не сделать")
 
-                case self.__third_rib:
-                    res = self.__first_rib + self.__second_rib
-                    if res > max_rib:
-                        print("Ура, можно построить треугольник!")
-                    else:
-                        print("Жаль, но из этого треугольник не сделать")
+                    case self.__third_rib:
+                        res = self.__first_rib + self.__second_rib
+                        if res > max_rib:
+                            print("Ура, можно построить треугольник!")
+                        else:
+                            print("Жаль, но из этого треугольник не сделать")
+            else:
+                print("С отрицательными числами ничего не выйдет!")
         else:
-            print("С отрицательными числами ничего не выйдет!")
+            print("Нужно вводить только числа!")
 
 
 if __name__ == "__main__":
@@ -70,12 +71,5 @@ if __name__ == "__main__":
 1) Ура, можно построить треугольник!
 2) Жаль, но из этого треугольник не сделать
 3) С отрицательными числами ничего не выйдет!
-4) 
-    Traceback (most recent call last):
-    File "/home/server/Documents/Two_Tasks/Task-2/main.py", line 65, in <module>
-        triangle = TriangleChecker(60, "Hello", 80)
-                ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
-    File "/home/server/Documents/Two_Tasks/Task-2/main.py", line 24, in __init__
-        raise TypeError("Нужно вводить только числа!")
-    TypeError: Нужно вводить только числа!
+4) Нужно вводить только числа!
 """
