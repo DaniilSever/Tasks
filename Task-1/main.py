c439a2c53336295ffb04675f3588537d5fc52b36
Server <server@localhost.localdomain>
2024-02-19 02:35:34 -0500

    fin

--- Task-1/main.py
+++ Task-1/main.py
@@ -7,3 +7,32 @@
 а иначе отобразится следующая фраза: Обычная газировка.
 
 """
+
+
+class Soda:
+    def __init__(self, supplement: str = None):
+        if isinstance(supplement, str):
+            self.__supplement = supplement
+        else:
+            self.__supplement = None
+
+    def show_my_drink(self):
+        if self.__supplement is None:
+            print("Обычная газировка")
+        else:
+            print(f"Газировка с добавкой '{self.__supplement}'")
+
+
+if __name__ == "__main__":
+    # 1) Обычная Газировка
+    drink = Soda()
+    drink.show_my_drink()
+    # 2) Газировка с добавкой
+    drink = Soda("Кокосовое молоко")
+    drink.show_my_drink()
+
+# OUTPUT
+"""
+1) Обычная газировка
+2) Газировка с добавкой 'Кокосовое молоко'
+"""
