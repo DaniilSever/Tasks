b885c5406e08e90b88d1d755e0e2ea86bb8c0738
Server <server@localhost.localdomain>
2024-02-24 06:39:19 -0500

    task-4 complite

--- Task-4/main.py
+++ Task-4/main.py
@@ -5,3 +5,21 @@
 – Микросекунда – текущая миллисекунда на часах.
 На каждой итерации цикла искусственно приостанавливайте скрипт на 0,01 секунды.
 """
+
+import csv
+import time
+from datetime import datetime as dtime
+
+with open('temp.csv', 'w', newline='') as csvfile:
+    names = ['№', 'sec', 'msec']
+    write = csv.DictWriter(csvfile, fieldnames=names)
+    sec_now, msec_now = dtime.now().second, dtime.now().microsecond
+    write.writeheader()
+    for i in range(1, 301):
+        time.sleep(0.01)
+        write.writerow({'№': i, 'sec': sec_now, 'msec': msec_now})
+
+with open('temp.csv', newline='') as csvfile:
+    reader = csv.reader(csvfile)
+    for row in reader:
+        print(row)
