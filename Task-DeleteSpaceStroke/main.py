d50cfcf7d1a3e62b82098bd412190715417c3b2c
Server <server@localhost.localdomain>
2024-02-29 06:43:52 -0500

    change file to pip8

--- Task-DeleteSpaceStroke/main.py
+++ Task-DeleteSpaceStroke/main.py
@@ -1,13 +1,12 @@
-def del_space_stroke(stroke: str): 
+def del_space_stroke(stroke: str):
     tmp = []
     for word in stroke:
         tmp.append(word)
 
-    while ' ' in tmp:
-        tmp.remove(' ')
-    
-    return ''.join(str(element) for element in tmp)
-    
+    while " " in tmp:
+        tmp.remove(" ")
+
+    return "".join(str(element) for element in tmp)
 
 
-print(del_space_stroke(' Hello World !  '))
+print(del_space_stroke(" Hello World !  "))
