a17f2b960be594095f72eb769244db846ccb66ac
Server <server@localhost.localdomain>
2024-02-29 06:41:47 -0500

    complete DeleteSpaceStroke

---
+++ Task-DeleteSpaceStroke/main.py
@@ -0,0 +1,13 @@
+def del_space_stroke(stroke: str): 
+    tmp = []
+    for word in stroke:
+        tmp.append(word)
+
+    while ' ' in tmp:
+        tmp.remove(' ')
+    
+    return ''.join(str(element) for element in tmp)
+    
+
+
+print(del_space_stroke(' Hello World !  '))
