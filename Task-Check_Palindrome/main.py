b1778f299ff87d69a044ccfd2508db89fcc3f645
Server <server@localhost.localdomain>
2024-02-29 06:34:44 -0500

    complete check_palindrome

---
+++ Task-Check_Palindrome/main.py
@@ -0,0 +1,21 @@
+def check_palindrome(word: str) -> str:
+    tmp, revers_tmp = [], []
+    for i in word:
+        tmp.append(i)
+
+    while " " in tmp:
+        tmp.remove(" ")
+
+    while "," in tmp:
+        tmp.remove(",")
+
+    tmp = [i.lower() for i in tmp]
+    revers_tmp = [i for i in tmp[::-1]]
+
+    if revers_tmp == tmp:
+        return "Строка является полиндромом"
+    else:
+        return "Строка не является полиндромом"
+
+
+print(check_palindrome("Яд Ефрему, а умер Федя"))
